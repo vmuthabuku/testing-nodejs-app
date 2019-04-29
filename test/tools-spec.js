7c439697697f8c2a81b3ac23ebb62f99d4127dc0
vmuthabuku <vmuthabuku@gmail.com>
2019-04-30 02:30:31 +0300

    --init-initial commit

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -4,7 +4,7 @@ chai.use(chaiHttp);
 let should = require('chai').should();
 let expect = require('chai').expect
 let mongoose = require("mongoose");
-let Todos = require('../models.js/todo')
+let Todos = require('../models.js/todo').Todo
 let app = require('../server');
 const config = require("../config/testDb").get(process.env.NODE_ENV)
 
@@ -12,6 +12,12 @@ mongoose.Promise = global.Promise
 mongoose.connect(config.DATABASE, { useNewUrlParser: true })
 
 
+describe('Books', () => {
+    beforeEach((done) => {
+        Todos.remove({}, (err) => { 
+           done();           
+        });        
+    });
 
 describe("Todos", () => {
   describe('/GET api/todos', () => {
@@ -35,15 +41,46 @@ describe("Todos", () => {
           .end((err,res) => {
               res.should.have.status(201);
               res.body.should.be.a('object')
-              //should(err === null).be.null
               should.exist(res.body);
               expect(todos).to.have.property('title')
               expect(todos).to.have.property('todo')
-              //res.body.todos.should.have.property('title')
-              //res.body.todos.should.have.property('todo')
               done();
 
           })
-      })
+      });
+      describe('/PUT /api/todoUpdate ', () => {
+        it('it should UPDATE a todo given the id', (done) => {
+            let todo = new Todos({title:"Test Update Me", todo:"try out updating an app"})
+            todo.save((err, todos) => {
+                  chai.request(app)
+                  .put(`/api/todoUpdate?=${todo.id}`)
+                  .send({title:"Test Me", todo:"try out updating me"})
+                  .end((err, res) => {
+                        res.should.have.status(201);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('Success').eql(true);
+                        
+                    done();
+                  });
+            });
+        });
+    });
+    describe('/DELETE /api/todoDelete', () => {
+        it('it should DELETE a todo given the id', (done) => {
+            let todo = new Todos({title:"Test Update Me", todo:"try out updating an app"})
+            todo.save((err, todo) => {
+                  chai.request(app)
+                  .delete(`/api/todoDelete?=${todo.id}`)
+                  .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('object');
+                        res.body.should.have.property('Success').eql(true);
+                    done();
+                  });
+            });
+        });
+    })
+
   });
+})
 })
